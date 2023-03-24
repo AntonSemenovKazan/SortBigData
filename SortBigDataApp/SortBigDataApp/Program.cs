06893b73c98aea71b7c1348431a8d7148fd6e273
Anton <menanton@gmail.com>
2023-03-24 11:03:24 +0300

    Init Console App

---
+++ SortBigDataApp/SortBigDataApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SortBigDataApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
